4b30faeb4e62b7b07d1b1b2fcd99c6f791db2c88
zenin5479 <zenin5479@gmail.com>
2025-06-23 09:39:41 +0300

    Update 2.5 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 // Разработать программу для обработки двух или трёх двумерных массивов разного размера с использованием подпрограмм
 // При разработке подпрограмм предусмотреть случай, когда искомых элементов нет
@@ -19,6 +20,28 @@ namespace Homework_4._7
          string nameOne = "A";
          string nameTwo = "B";
          string nameThree = "C";
+
+         string pathOne = Path.GetFullPath("a.txt");
+         if (!File.Exists(pathOne))
+         {
+            Console.WriteLine("Ошибка при открытии файла для чтения. Файл не существует");
+         }
+
+         string pathTwo = Path.GetFullPath("b.txt");
+         if (!File.Exists(pathTwo))
+         {
+            Console.WriteLine("Ошибка при открытии файла для чтения. Файл не существует");
+         }
+
+         string pathThree = Path.GetFullPath("c.txt");
+         if (!File.Exists(pathThree))
+         {
+            Console.WriteLine("Ошибка при открытии файла для чтения. Файл не существует");
+         }
+
+
+
+
 
          int rowOne = VariousMethods.SizeRow(nameOne);
          int columnOne = VariousMethods.SizeColumn(nameOne);
