696da196daeadac7c9430836b009bdeec1e60a6a
zenin5479 <zenin5479@gmail.com>
2025-06-23 09:48:11 +0300

    Update 2.9 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -81,7 +81,7 @@ namespace Homework_4._7
          }
          else
          {
-            double[] searchOne = VariousMethods.InputArray(sourceOne, elementsOne, nameOne);
+            double[,] searchOne = VariousMethods.InputArray(sourceOne, rowOne, columnOne);
             double maxOne = VariousMethods.FindMaxArray(searchOne, nameOne);
             double[] replacingOne = VariousMethods.ReplacingMax(searchOne, maxOne);
             string[] arrayOne = VariousMethods.VivodStringArray(replacingOne);
