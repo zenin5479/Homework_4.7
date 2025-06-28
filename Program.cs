90d63bcd455c6849cb0c3d4ce7500938de711ace
zenin5479 <zenin5479@gmail.com>
2025-06-28 10:02:03 +0300

    Update 5.7 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -90,5 +90,31 @@ namespace Homework_4._7
 
          Console.ReadKey();
       }
+
+
+      bool find_positiv(double** x, int n, int m, bool& fl)
+      {
+         int i = -1;
+         while (i < n and fl)
+
+         {
+            i++;
+            int j = 0;
+            while (j < m and fl)
+
+            {
+               if (x[i][j] > 0)
+               {
+                  fl = false;
+               }
+               else
+               {
+                  j++;
+               }
+            }
+         }
+
+         return fl;
+      }
    }
 }
