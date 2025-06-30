f8694609e6b3dbc3ed1ab941106dd34844a3d7d9
zenin5479 <zenin5479@gmail.com>
2025-06-30 19:49:54 +0300

    Update 12.5 Обновлен метод. Проведен рефакторинг.

--- Program.cs
+++ Program.cs
@@ -64,6 +64,27 @@ namespace Homework_4._7
             }
          }
 
+         //double minTwo = -1.0;
+         //double[,] sourceTwo = VariousMethods.VvodArray(pathTwo);
+         //if (sourceTwo.GetLength(0) == 0)
+         //{
+         //   Console.WriteLine("Файл пуст");
+         //}
+         //else
+         //{
+         //   double[,] searchTwo = VariousMethods.InputArray(sourceTwo, rowTwo, columnTwo, nameTwo);
+         //   bool flTwo = VariousMethods.SearchingPositiv(searchTwo);
+         //   if (flTwo)
+         //   {
+         //      Console.WriteLine("В двумерном массиве {0} нет искомых положительных элементов", nameTwo);
+         //   }
+         //   else
+         //   {
+         //      minTwo = VariousMethods.SearchingMinPositiv(searchTwo);
+         //      Console.WriteLine(minTwo);
+         //   }
+         //}
+
          double minTwo = -1.0;
          double[,] sourceTwo = VariousMethods.VvodArray(pathTwo);
          if (sourceTwo.GetLength(0) == 0)
@@ -84,6 +105,7 @@ namespace Homework_4._7
                Console.WriteLine(minTwo);
             }
          }
+
 
          // Сравниваем значения double используя метод CompareTo(Double) 
          //if (minOne.CompareTo(-1.0) == 0)
