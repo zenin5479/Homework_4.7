290e46ffd8ff50fb3f4327feeb6e788ce978441a
zenin5479 <zenin5479@gmail.com>
2025-06-30 20:29:01 +0300

    Update 13.7 Обновлен метод. Проведен рефакторинг.

--- Program.cs
+++ Program.cs
@@ -107,9 +107,9 @@ namespace Homework_4._7
          }
 
          double result = CalculatingValue(minOne, minTwo, minThree);
-         Console.WriteLine("Результат: {0}", result);
-         //Console.WriteLine("Результат: {0:f2}", result);
-         //Console.WriteLine("Результат: {0:f}", result);
+         Console.WriteLine("Результат Amin * Bmin – Cmin: {0}", result);
+         //Console.WriteLine("Результат Amin * Bmin – Cmin: {0:f2}", result);
+         //Console.WriteLine("Результат Amin * Bmin – Cmin: {0:f}", result);
 
          //string[] stringArray = VariousMethods.VivodStringArray(maxOne);
          //VariousMethods.FileWriteString(stringArray);
